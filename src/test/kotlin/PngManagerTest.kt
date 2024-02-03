ad497b4e08539e79823013b532ba41b93f2cc141
Arthur <arthrp@users.noreply.github.com>
2024-02-03 23:21:17 +0200

    Test

--- src/test/kotlin/PngManagerTest.kt
+++ src/test/kotlin/PngManagerTest.kt
@@ -1,6 +1,7 @@
 import org.arthrp.PngManager
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
 import java.io.File
 import kotlin.test.assertFailsWith
 
@@ -15,5 +16,17 @@ internal class PngManagerTest {
             p.parse(file.absolutePath)
         }
         assertEquals(exception.message, "Not a valid png file!")
+    }
+
+    @Test
+    fun loadingSmallByteArrAsPngFails(){
+        val arr = byteArrayOf(0x89.toByte(), 0x50.toByte(), 0x4E.toByte(), 0x47.toByte(), 0x00.toByte())
+        val inputStream = ByteArrayInputStream(arr)
+
+        val p = PngManager()
+        val exception = assertFailsWith<IllegalArgumentException> {
+            p.parse(inputStream)
+        }
+        assertEquals(exception.message, "File too small, not a valid png")
     }
 }
